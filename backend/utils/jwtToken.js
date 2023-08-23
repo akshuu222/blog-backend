f61850e320b8d70b2e4413f4b6701425a73a74a8
akshuu222 <118178688+akshuu222@users.noreply.github.com>
2023-08-23 21:47:06 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -4,6 +4,7 @@ const sendToken = async (user, res, status, message, next) => {
 
     const options = {
       httpOnly: true,
+      SameSite:"None",
       expires: new Date(
         Date.now() + Number(process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000
       ),
